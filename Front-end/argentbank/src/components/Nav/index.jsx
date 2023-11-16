1a8964121d7a6e0c5fe8361e1492b72c38927245
Mathias <mthsvilly@gmail.com>
2023-11-16 15:46:51 +0100

    Navigation et css de la Page error404

--- Front-end/argentbank/src/components/Nav/index.jsx
+++ Front-end/argentbank/src/components/Nav/index.jsx
@@ -1,22 +1,23 @@
+import { Link } from "react-router-dom";
 import ArgentBankLogo from "../../assets/argentBankLogo.png";
 import "./nav.css";
 
 function Nav() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src={ArgentBankLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <Link to="/sign-in" className="main-nav-item">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   );
