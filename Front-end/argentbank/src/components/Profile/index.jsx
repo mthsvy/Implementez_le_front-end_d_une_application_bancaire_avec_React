805fdcb114a3c0c299e3132c37faf0ca05d9801d
Mathias <mthsvilly@gmail.com>
2023-11-23 08:27:18 +0100

    Mise en place du formulaire

--- Front-end/argentbank/src/components/Profile/index.jsx
+++ Front-end/argentbank/src/components/Profile/index.jsx
@@ -1,15 +1,73 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
+import "./profile.css";
 
 function Profile() {
   const user = useSelector((state) => state.token);
+  console.log(user);
+  const [display, setDisplay] = useState(true);
+  const [username, setUserName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const updateUserName = () => {};
   return (
     <div className="header">
-      <h1>
-        Welcome back
-        <br />
-        {user.firstname} {user.lastname}
-      </h1>
-      <button className="edit-button">Edit Name</button>
+      {display ? (
+        <div>
+          <h2>
+            Welcome back
+            <br />
+            {user.firstname} {user.lastname} !
+          </h2>
+          <button className="edit-button" onClick={() => setDisplay(!display)}>
+            Edit Name
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>Edit Profile</h2>
+          <form>
+            <div className="edit-input">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                defaultValue={user.username}
+                onChange={(event) => setUserName(event.target.value)}
+              />
+            </div>
+            <div className="edit-input">
+              <label htmlFor="firstname">First name:</label>
+              <input
+                type="text"
+                id="firstname"
+                defaultValue={user.firstname}
+                disabled={true}
+              />
+            </div>
+            <div className="edit-input">
+              <label htmlFor="lastname">Last name:</label>
+              <input
+                type="text"
+                id="lastname"
+                defaultValue={user.lastname}
+                disabled={true}
+              />
+            </div>
+            <div className="buttons">
+              <button className="edit-username-button" onClick={updateUserName}>
+                Save
+              </button>
+              <button
+                className="edit-username-button"
+                onClick={() => setDisplay(!display)}
+              >
+                Cancel
+              </button>
+            </div>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+          </form>
+        </div>
+      )}
     </div>
   );
 }
